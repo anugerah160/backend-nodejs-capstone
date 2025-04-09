07711985ffc76c9f56a8e718b1158e807d7a7e39
anugerah160 <artuphareguna10@gmail.com>
2025-04-09 03:17:53 -0400

    Search API done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -6,6 +6,7 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+const searchRoutes = require("./routes/searchRoutes");
 
 
 const app = express();
@@ -22,16 +23,15 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 const pinoHttp = require('pino-http');
