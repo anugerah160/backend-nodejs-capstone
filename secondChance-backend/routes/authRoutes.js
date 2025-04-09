a3d753ffd5743bd76cd8d4d05a05977fbbe6f1e7
anugerah160 <artuphareguna10@gmail.com>
2025-04-09 10:05:18 -0400

    Back-end Integration for User Profile

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const connectToDatabase = require('../models/db');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 const logger = require('../logger');
 const bcrypt = require('bcryptjs/dist/bcrypt');
 require('dotenv').config();
@@ -110,6 +111,65 @@ router.post('/login', async (req, res) => {
     } catch (e) {
          return res.status(500).send('Internal server error');
     }
+});
+
+router.put('/update', [
+    body('firstName').optional().isLength({ min: 2 }).withMessage('First name must be at least 2 characters'),
+    body('lastName').optional().isLength({ min: 2 }).withMessage('Last name must be at least 2 characters'),
+    body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  ],async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      logger.warn('Validation errors:', errors.array());
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+    const userEmail = req.header('email');
+      if (!userEmail) {
+        logger.error('Email header is missing');
+        return res.status(400).json({ error: 'Email header is required' });
+    }
+    // Task 4: Connect to MongoDB
+    const db = await connectToDatabase();
+    const collection = db.collection('users');
+    // Task 5: Find the user credentials in database
+    const existingUser = await collection.findOne({ email: userEmail });
+    if (!existingUser) {
+        logger.error('User not found');
+        return res.status(404).json({ error: 'User not found' });
+        }
+    // Prepare updated fields
+    const updatedFields = {};
+    if (req.body.firstName) updatedFields.firstName = req.body.firstName;
+    if (req.body.lastName) updatedFields.lastName = req.body.lastName;
+    if (req.body.password) {
+      const salt = await bcryptjs.genSalt(10);
+      const hash = await bcryptjs.hash(req.body.password, salt);
+      updatedFields.password = hash;
+    }
+
+    updatedFields.updatedAt = new Date();
+    // Task 6: Update the user credentials in the database
+    await collection.updateOne(
+        {email: userEmail},
+        {$set: updatedFields}
+    )
+
+    // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+    const payload = {
+        user: {
+          id: existingUser._id,
+        },
+      };
+    const authtoken = jwt.sign(payload, JWT_SECRET);  
+    logger.info('User updated successfully');
+    res.json({authtoken});
+} catch (e) {
+    logger.error(`Error in update: ${e}`);
+    return res.status(500).send('Internal server error');
+}
 });
 
 module.exports = router;
